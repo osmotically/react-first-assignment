e8cb7ec0a6eb6e08312de01826a3a0d864b117f6
Lee <osmotically@gmail.com>
2020-07-12 17:01:40 -0700

    Updates

---
+++ precache-manifest.5a93ab4ab215039e4263ec3418e65139.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05d5dfe0cfeef766a47bdc0edaaf2ca",
+    "url": "/react-first-assignment/index.html"
+  },
+  {
+    "revision": "cea3f1a91f0e30545338",
+    "url": "/react-first-assignment/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "93876dc00614e1819f29",
+    "url": "/react-first-assignment/static/js/2.2b5c4290.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-first-assignment/static/js/2.2b5c4290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea3f1a91f0e30545338",
+    "url": "/react-first-assignment/static/js/main.db6aeed6.chunk.js"
+  },
+  {
+    "revision": "42b5b6ac9237e209b97d",
+    "url": "/react-first-assignment/static/js/runtime-main.000789c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-first-assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
